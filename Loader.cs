80e7b2425804337673f688276a88c85c363baefd
Ammonite616 <89497089+Ammonite616@users.noreply.github.com>
2025-10-02 12:09:37 -0400

    Add files via upload

---
+++ Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace plentifulseas
+{
+	public class Loader
+	{
+		/// <summary>
+		/// This method is run by Winch to initialize your mod
+		/// </summary>
+		public static void Initialize()
+		{
+			var gameObject = new GameObject(nameof(plentifulseas));
+			gameObject.AddComponent<plentifulseas>();
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
---
+++ plentifulseas.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace plentifulseas
+{
+	public class plentifulseas : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(plentifulseas)} has loaded!");
+		}
+	}
+}
